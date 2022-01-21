c0c15e262feab1ed0c9ab866cf77b4aa0648b58a
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-01-21 20:50:59 +0700

    [DEV] update deph

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
-  env: {
+  'env': {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "google"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
+  'extends': ['plugin:react/recommended', 'google'],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 13,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {
-    "react/react-in-jsx-scope": "off",
+  'plugins': ['react', '@typescript-eslint'],
+  'rules': {
+    'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
+    'react/jsx-filename-extension': [1, {extensions: ['.js', '.jsx', '.ts', '.tsx']}],
     // should add ".ts" if typescript project
-    "react/prop-types": "off",
-    "require-jsdoc": "off",
-    "max-len": "off",
+    'react/prop-types': 'off',
+    'require-jsdoc': 'off',
+    'max-len': 'off',
   },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+
 };
