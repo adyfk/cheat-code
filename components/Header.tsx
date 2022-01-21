5ea714a24a8ed867ac25d44c016d963ff287adbb
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-01-21 20:54:39 +0700

    [DEV] add routing to github

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
         </a>
       </Link>
       <div>
-        <a href="" className='text-sm font-semibold cursor-pointer text-sky-700 hover:text-sky-500'>
+        <a href="https://github.com/adyfk/cheat-code" className='text-sm font-semibold cursor-pointer text-sky-700 hover:text-sky-500'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
           </svg>
