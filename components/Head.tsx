2e57b7772f0a583008218ad05002579aeaaa533a
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-01-28 00:00:12 +0700

    [CONFIG] fix header

--- components/Head.tsx
+++ components/Head.tsx
@@ -3,7 +3,7 @@ import NextHead from 'next/head';
 const Head: React.FC<{title?: string; desc?: string}> = ({title, desc='cheatsheet code that saves you from having to remember everything you learn'}) => {
   return (
     <NextHead>
-      <title>CheatCode {!!title && `- ${title}`}</title>
+      <title>CheatCode {!!title ? `- ${title}`: ''}</title>
       <meta name="description" content={desc} />
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </NextHead>
