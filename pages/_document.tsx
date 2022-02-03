996bb18705b41156978a4f86d54729f1f835d23e
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-02-03 07:30:36 +0700

    [CONFIG] update import font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,9 +13,6 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta name="google-site-verification" content="pqURzWqJv4aLGt93AqrLuOZ4R0ZxbwlJymFj1mnpkSo" />
-          <style>
-            @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap');
-          </style>
         </Head>
         <body className='bg-slate-50'>
           <Main></Main>
