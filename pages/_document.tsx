87764e6d989e3e328357cac895c5941f4036d78c
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-02-06 01:11:47 +0700

    [CONFIG] update meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,9 +13,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <link rel='shortcut icon' href='/favicon.ico' />
-          <meta name="google-site-verification" content="pqURzWqJv4aLGt93AqrLuOZ4R0ZxbwlJymFj1mnpkSo" />
           <link rel='manifest' href='/manifest.json' />
-
           <meta name='application-name' content='Cheat Code' />
           <meta name='apple-mobile-web-app-capable' content='yes' />
           <meta name='apple-mobile-web-app-status-bar-style' content='default' />
@@ -23,8 +21,9 @@ class MyDocument extends Document {
           <meta name='mobile-web-app-capable' content='yes' />
           <meta name="theme-color" content="#FEFEFE"/>
           <link rel="apple-touch-icon" href={`${process.env.BASEURL}/icon-192x192.png`} />
-
           <link rel='icon' type='image/png' sizes='32x32' href='icon-32x32.png' />
+          <meta name="google-site-verification" content="pqURzWqJv4aLGt93AqrLuOZ4R0ZxbwlJymFj1mnpkSo" />
+          <meta name="yandex-verification" content="662f5acd93fe5c17" />
         </Head>
         <body className='bg-slate-50'>
           <Main></Main>
