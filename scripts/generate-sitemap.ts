c12987e9817f3e390d9190388e30f84e0f194b97
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-02-07 13:00:25 +0700

    [DEV] update generate sitemap

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -2,12 +2,13 @@ import {getDate} from '@utils/get-date';
 import {writeFileSync, rmSync} from 'fs';
 
 export async function generate(posts: any) {
+  const nowDate = new Date();
   const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
         <url>
             <loc>${baseUrl}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
+            <lastmod>${nowDate.toISOString()}</lastmod>
             <priority>1.00</priority>
         </url>
         ${posts.map(({slug, data}:any) => {
@@ -23,11 +24,17 @@ export async function generate(posts: any) {
     </urlset>`;
   try {
     rmSync('public/sitemap.xml', {recursive: true, force: true});
-  } catch (error) {
-  }
+  } catch (error) {}
+  try {
+    rmSync(`public/sitemap-${nowDate.getMonth()-1}.xml`, {recursive: true, force: true});
+  } catch (error) {}
+  try {
+    rmSync(`public/sitemap-${nowDate.getMonth()}.xml`, {recursive: true, force: true});
+  } catch (error) {}
 
   try {
-    writeFileSync('public/sitemap.xml', sitemap);
+    writeFileSync(`public/sitemap.xml`, sitemap);
+    writeFileSync(`public/sitemap-${nowDate.getMonth()}.xml`, sitemap);
     console.log('success generating sitemap');
   } catch (error) {
     console.error('error generating sitemap');
