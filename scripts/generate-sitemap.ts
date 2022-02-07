ba971aff603d848307d771e479cc965a1d91ca39
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-02-07 11:01:14 +0700

    [CONFIG] update sitemap

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -4,16 +4,18 @@ import {writeFileSync, rmSync} from 'fs';
 export async function generate(posts: any) {
   const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
         <url>
             <loc>${baseUrl}</loc>
             <lastmod>${new Date().toISOString()}</lastmod>
+            <priority>1.00</priority>
         </url>
         ${posts.map(({slug, data}:any) => {
     return `
             <url>
                 <loc>${baseUrl+'/'+slug}</loc>
                 <lastmod>${getDate(data.updateAt)}</lastmod>
+                <priority>0.80</priority>
             </url>
             `;
   })
