6d6ce1befca04633f697bfab15573f425a9ddeb8
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2022-02-07 16:00:28 +0700

    [DEV] update sitemap

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -1,26 +1,31 @@
 import {getDate} from '@utils/get-date';
 import {writeFileSync, rmSync} from 'fs';
+import prettier from 'prettier';
 
+const formatted = (sitemap:string) => prettier.format(sitemap, {parser: 'html'});
 export async function generate(posts: any) {
   const nowDate = new Date();
   const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-        <url>
-            <loc>${baseUrl}</loc>
-            <lastmod>${nowDate.toISOString()}</lastmod>
-            <priority>1.00</priority>
-        </url>
-        ${posts.map(({slug, data}:any) => {
+
+  const postListSitemap = posts.map(({slug, data}:any) => {
     return `
             <url>
                 <loc>${baseUrl+'/'+slug}</loc>
                 <lastmod>${getDate(data.updateAt)}</lastmod>
+                <changefreq>monthly</changefreq>
                 <priority>0.80</priority>
             </url>
             `;
-  })
-      .join('')}
+  }).join('');
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+    >
+        ${postListSitemap}
     </urlset>`;
   try {
     rmSync('public/sitemap.xml', {recursive: true, force: true});
@@ -33,8 +38,9 @@ export async function generate(posts: any) {
   } catch (error) {}
 
   try {
-    writeFileSync(`public/sitemap.xml`, sitemap);
-    writeFileSync(`public/sitemap-${nowDate.getMonth()}.xml`, sitemap);
+    const formateedSitemap: any = formatted(sitemap);
+    writeFileSync(`public/sitemap.xml`, formateedSitemap, 'utf8');
+    writeFileSync(`public/sitemap-${nowDate.getMonth()}.xml`, formateedSitemap, 'utf8');
     console.log('success generating sitemap');
   } catch (error) {
     console.error('error generating sitemap');
